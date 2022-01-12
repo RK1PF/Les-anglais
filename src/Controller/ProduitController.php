a3ab0d371868f7a778ac514fef0233c6f7705d9c
cjean29 <85945885+cjean29@users.noreply.github.com>
2022-01-12 16:56:47 +0100

    Add files via upload

---
+++ src/Controller/ProduitController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+use Symfony\Component\HttpFoundation\Request;
+
+use Doctrine\ORM\EntityManagerInterface ;
+
+
+use App\Repository\ProduitRepository;
+use App\Entity\Produit;
+use App\Form\ProduitType;
+
+class ProduitController extends AbstractController
+{
+    #[Route('/produit', name: 'produit')]
+    public function index(ProduitRepository $repository): Response
+    {
+        $repository = $this->getDoctrine()->getRepository(Produit::class);
+        $produit = $repository->findAll();
+        return $this->render('produit/index.html.twig', [
+                'produit' => $produit,
+            ]);
+    }
+
+    #[Route('/afficherproduit/{id}', name: 'afficheproduit')]
+
+    public function afficherUnProduit(ProduitRepository $pr,$id) : Response {
+        $produit = $pr->find($id);
+        return $this->render('produit/afficherUnProduit.html.twig', ["produit"=>$produit]);
+    }
+/*
+    #[Route("/modifproduit/{id}", name="modifproduit")]
+    #[Route("/creationproduit", name="creationproduit")]
+
+    public function modifierProduit(Produit $produit=null, Request $request, EntityManagerInterface $em) {
+        if (!$produit){
+            $produit=new Produit();
+        }
+        $form=$this->createForm(ProduitType::class, $produit);
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            $em->persist($produit);
+            $em->flush();
+            return $this->redirectToRoute('produit');
+        }
+        return $this->render('produit/modifProduit.html.twig', [
+            "produit"=>$produit, 
+            "form"=>$form->createView()
+        ]);
+    } */
+}
