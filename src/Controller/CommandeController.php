89e73a2707bb644b6437dd1a242e1ea9a88c7e05
cjean29 <cocojean94@hotmail.fr>
2022-01-20 16:57:16 +0100

    controller mis à jour

--- src/Controller/CommandeController.php
+++ src/Controller/CommandeController.php
@@ -7,15 +7,21 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 use App\Repository\CommandeRepository;
+use App\Repository\ProduitCommandeRepository;
+use App\Repository\ProduitRepository;
 
 
 class CommandeController extends AbstractController
 {
     #[Route('/commande/{id}', name: 'commande')]
-    public function commande(CommandeRepository $cr,$id) : Response {
+    public function commande(CommandeRepository $cr, ProduitCommandeRepository $pcr,ProduitRepository $pr,$id) : Response {
         $commande = $cr->find($id);
+        $produitCommande = $pcr->find($id);
+        $produit = $pr->find($id);
         return $this->render('commande/factures.html.twig', [
-            "commande"=>$commande
+            "commande"=>$commande,
+            "produitcommande"=>$produitCommande,
+            "produit"=>$produit
         ]);
     }
 
