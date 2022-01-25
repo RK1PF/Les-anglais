03b52268e28e4f79dc63fbf9bee481e222a58ffb
RK1PF <graffe.raihau@gmail.com>
2022-01-25 22:49:27 +0100

    format, indentation

--- src/Controller/CommandeController.php
+++ src/Controller/CommandeController.php
@@ -14,50 +14,52 @@ use App\Repository\ProduitRepository;
 class CommandeController extends AbstractController
 {
     #[Route('/gestion_commande', name: 'gestion_commande')]
-    public function index(CommandeRepository $cr, ProduitCommandeRepository $pcr,ProduitRepository $pr,$id) : Response {
+    public function index(CommandeRepository $cr, ProduitCommandeRepository $pcr, ProduitRepository $pr, $id): Response
+    {
         $commande = $cr->find($id);
         $produitCommande = $pcr->find($id);
         $produit = $pr->find($id);
         return $this->render('commande/commande.html.twig', [
-            "commande"=>$commande,
-            "produitcommande"=>$produitCommande,
-            "produit"=>$produit
+            "commande" => $commande,
+            "produitcommande" => $produitCommande,
+            "produit" => $produit
         ]);
-    }    
-
+    }
 
     #[Route('/commande/{id}', name: 'commande')]
-    public function commande(CommandeRepository $cr, ProduitCommandeRepository $pcr,ProduitRepository $pr,$id) : Response {
+    public function commande(CommandeRepository $cr, ProduitCommandeRepository $pcr, ProduitRepository $pr, $id): Response
+    {
         $commande = $cr->find($id);
         $produitCommande = $pcr->find($id);
         $produit = $pr->find($id);
         return $this->render('commande/commande.html.twig', [
-            "commande"=>$commande,
-            "produitcommande"=>$produitCommande,
-            "produit"=>$produit
+            "commande" => $commande,
+            "produitcommande" => $produitCommande,
+            "produit" => $produit
         ]);
     }
 
 
     #[Route('/factures', name: 'factures')]
-    public function factures(CommandeRepository $cr, ProduitCommandeRepository $pcr,ProduitRepository $pr,$id) : Response {
+    public function factures(CommandeRepository $cr, ProduitCommandeRepository $pcr, ProduitRepository $pr, $id): Response
+    {
         $commande = $cr->find($id);
         $produitCommande = $pcr->find($id);
         $produit = $pr->find($id);
         return $this->render('commande/commande.html.twig', [
-            "commande"=>$commande,
-            "produitcommande"=>$produitCommande,
-            "produit"=>$produit
+            "commande" => $commande,
+            "produitcommande" => $produitCommande,
+            "produit" => $produit
         ]);
     }
 
 
     #[Route('/paiement', name: 'paiement')]
-    public function paiement(CommandeRepository $cr,$id): Response
+    public function paiement(CommandeRepository $cr, $id): Response
     {
         $commande = $cr->find($id);
         return $this->render('commande/paiement.html.twig', [
-            "commande"=>$commande
+            "commande" => $commande
         ]);
     }
 }
