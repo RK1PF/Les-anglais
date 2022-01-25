5c41b61b2adecfb86f0e1817997f8cc636bf1c81
cjean29 <cocojean94@hotmail.fr>
2022-01-25 16:31:36 +0100

    commande_controller

--- src/Controller/CommandeController.php
+++ src/Controller/CommandeController.php
@@ -13,57 +13,51 @@ use App\Repository\ProduitRepository;
 
 class CommandeController extends AbstractController
 {
+    #[Route('/gestion_commande', name: 'gestion_commande')]
+    public function index(CommandeRepository $cr, ProduitCommandeRepository $pcr,ProduitRepository $pr,$id) : Response {
+        $commande = $cr->find($id);
+        $produitCommande = $pcr->find($id);
+        $produit = $pr->find($id);
+        return $this->render('commande/commande.html.twig', [
+            "commande"=>$commande,
+            "produitcommande"=>$produitCommande,
+            "produit"=>$produit
+        ]);
+    }    
+
+
     #[Route('/commande/{id}', name: 'commande')]
     public function commande(CommandeRepository $cr, ProduitCommandeRepository $pcr,ProduitRepository $pr,$id) : Response {
         $commande = $cr->find($id);
         $produitCommande = $pcr->find($id);
         $produit = $pr->find($id);
-        return $this->render('commande/factures.html.twig', [
+        return $this->render('commande/commande.html.twig', [
             "commande"=>$commande,
             "produitcommande"=>$produitCommande,
             "produit"=>$produit
         ]);
     }
 
-    #[Route('/commande_test', name: 'commande_test')]
-    public function index(): Response
-    {
-        return $this->render('commande/commande_test.html.twig', [
-            'controller_name' => 'CommandeController',
-        ]);
-    }
 
     #[Route('/factures', name: 'factures')]
-    // public function index(): Response
-
-    public function factures(): Response
-    {
-        return $this->render('commande/factures.html.twig', [
-            'controller_name' => 'CommandeController',
-        ]);
-    }
-
-    #[Route('/gestion_commande', name: 'gestion_commande')]
-    public function gestion_commande(): Response
-    {
+    public function factures(CommandeRepository $cr, ProduitCommandeRepository $pcr,ProduitRepository $pr,$id) : Response {
+        $commande = $cr->find($id);
+        $produitCommande = $pcr->find($id);
+        $produit = $pr->find($id);
         return $this->render('commande/commande.html.twig', [
-            'controller_name' => 'CommandeController',
+            "commande"=>$commande,
+            "produitcommande"=>$produitCommande,
+            "produit"=>$produit
         ]);
     }
+
 
     #[Route('/paiement', name: 'paiement')]
-    public function paiement(): Response
+    public function paiement(CommandeRepository $cr,$id): Response
     {
+        $commande = $cr->find($id);
         return $this->render('commande/paiement.html.twig', [
-            'controller_name' => 'CommandeController',
-        ]);
-    }
-
-    #[Route('/gestion_test', name: 'gestion_test')]
-    public function gestion_test(): Response
-    {
-        return $this->render('commande/gestion_test.html.twig', [
-            'controller_name' => 'CommandeController',
+            "commande"=>$commande
         ]);
     }
 }
