09266b88c62681956c49470d15b4c69cfbcae3ec
cjean29 <cocojean94@hotmail.fr>
2022-02-16 15:17:30 +0100

    Création commerce

---
+++ src/Controller/CommerceController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CommerceController extends AbstractController
+{
+    #[Route('/commerce', name: 'commerce')]
+    public function index(): Response
+    {
+        return $this->render('commerce/index.html.twig', [
+            'controller_name' => 'CommerceController',
+        ]);
+    }
+}
