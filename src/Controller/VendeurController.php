427ad3407eb4bd88fc841b29ade4d95a99cd2aee
cjean29 <cocojean94@hotmail.fr>
2022-02-16 15:41:37 +0100

    Vendeur

---
+++ src/Controller/VendeurController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class VendeurController extends AbstractController
+{
+    #[Route('/vendeur', name: 'vendeur')]
+    public function index(): Response
+    {
+        return $this->render('vendeur/index.html.twig', [
+            'controller_name' => 'VendeurController',
+        ]);
+    }
+}
